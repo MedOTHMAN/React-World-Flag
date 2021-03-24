0d8cffb9b34f42edfe3c588b51004d20366c8fd4
MedOTHMAN <mohamed.n.othman@gmail.com>
2021-03-24 17:34:22 +0100

    third

--- src/component/Countries.js
+++ src/component/Countries.js
@@ -4,24 +4,66 @@ import Card from "./Card";
 
 const Countries = () => {
   const [data, setData] = useState([]);
-
+  const [SortedData, setSortedData] = useState([]);
+  const [playOnce, setPlayOnce] = useState(true);
+  const [rangeValue, setRangeValue] = useState(40);
+  const [selectedRadio, setSelectedRadio] = useState("");
+  const radios = ['Africa','America','Asia','Europe','Oceania' ]
 
   useEffect(() => {
-        axios.get("https://restcountries.eu/rest/v2/all?fields=name;population;region;capital;flag")
-        .then((res) => setData(res.data));
+    if (playOnce) {
+      axios
+        .get(
+          "https://restcountries.eu/rest/v2/all?fields=name;population;region;capital;flag"
+        )
+        .then((res) => {
+          setData(res.data);
+          setPlayOnce(false);
+        });
+    }
 
-  }, [])
+    const sortedCountry = () => {
+      const countryObj = Object.keys(data).map((i) => data[i]);
+      const sortedArray = countryObj.sort((a, b) => {
+        return b.population - a.population;
+      });
+      sortedArray.length = rangeValue;
+      setSortedData(sortedArray);
+    };
+    sortedCountry();
+  }, [data, rangeValue, playOnce]);
 
   return (
     <div className="countries">
-        <ul className="countries-list">
-            {data.map((el)=>(
-                <Card country={el} key={el.name} />  
+      <div className="sort-container">
+        <input type="range" min="1" max="250" value={rangeValue}
+          onChange={(e) => setRangeValue(e.target.value)}
+        />
+        <ul>
+          {radios.map((radio)=>{
+            return (
+              <li key={radio}>
+                <input 
+                type='radio' 
+                value={radio} 
+                id='radio' 
+                checked={radio===selectedRadio}
+                onChange={(e)=>setSelectedRadio(e.target.value)}                
+                />
+                <label htmlFor={radio}>{radio}</label>
+              </li>
             )
-            )
-            }
+          })}
         </ul>
-
+        </div>
+        <div className="cancel">
+          {selectedRadio&&<h5 onClick={()=>setSelectedRadio("")} >Annuler Recherche</h5>}
+        </div>
+        <ul className="countries-list">
+            {SortedData.filter((country=>country.region.includes(selectedRadio))).map((el)=>(
+                <Card country={el} key={el.name} />  
+            ))}
+        </ul>
     </div>
   );
 };
