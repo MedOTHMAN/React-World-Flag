96e05652bbffac843620b290026915bca2ad7723
MedOTHMAN <mohamed.n.othman@gmail.com>
2021-03-30 17:13:48 +0100

    second

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Countries from '../component/Countries'
+import Logo from '../component/Logo'
 import Navigation from '../component/Navigation'
 
 const Home = () => {
     return (
         <div className="home" >
+            <Logo />
             <Navigation />
             <Countries />
         </div>
